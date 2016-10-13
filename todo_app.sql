f43e4f6b025d34b780de09a35ae7e2c25c005080
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2016-10-12 16:28:15 -1000

    finished

---
+++ todo_app.sql
@@ -0,0 +1,92 @@
+\c janphillipcarreon;
+
+DROP USER IF EXISTS michael;
+
+CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
+
+DROP DATABASE IF EXISTS todo_app;
+
+CREATE DATABASE todo_app;
+
+\c todo_app;
+
+CREATE TABLE tasks (
+  id serial,
+  title varchar(255),
+  description text,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp,
+  completed boolean
+);
+
+ALTER TABLE tasks DROP COLUMN completed;
+
+ALTER TABLE tasks ADD COLUMN completed_at timestamp;
+
+ALTER TABLE tasks
+  ALTER COLUMN completed_at SET DEFAULT NULL;
+
+ALTER TABLE tasks
+  ALTER COLUMN updated_at SET NOT NULL;
+
+ALTER TABLE tasks
+  ALTER COLUMN updated_at SET DEFAULT now();
+
+INSERT INTO tasks (id, title, description, created_at, updated_at, completed_at) VALUES (
+  default,
+  'Study SQL',
+  'Complete this exercise',
+  now(),
+  now(),
+  NULL
+);
+
+INSERT INTO tasks (title, description) VALUES (
+  'Study PostgreSQL',
+  'Read all the documentation'
+);
+
+SELECT title FROM tasks
+WHERE completed_at is NULL;
+
+UPDATE tasks
+SET completed_at = now()
+WHERE title = 'Study SQL';
+
+SELECT title FROM tasks
+WHERE completed_at is NULL;
+
+SELECT * FROM tasks
+ORDER BY created_at DESC;
+
+INSERT INTO tasks(title, description) VALUES (
+  'mistake 1',
+  'a test entry'
+
+);INSERT INTO tasks(title, description) VALUES (
+  'mistake 2',
+  'another test entry'
+
+);INSERT INTO tasks(title, description) VALUES (
+  'third mistake',
+  'another test entry'
+);
+
+SELECT title FROM tasks
+WHERE title like '%mistake%';
+
+DELETE
+FROM tasks
+WHERE title = 'mistake 1';
+
+SELECT title FROM tasks
+WHERE title like '%mistake%';
+
+DELETE
+FROM tasks
+WHERE title like '%mistake%';
+
+SELECT * FROM tasks
+ORDER BY created_at ASC;
+
+SELECT * FROM tasks;
